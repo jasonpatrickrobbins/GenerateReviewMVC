df9b45d0f8a6e718ffe3bc390c937ea99c8099bc
Jason Robbins <jasonrobbins@jasons-mbp-2.lan>
2021-09-13 08:24:54 -0400

    Added an NUnit testing project to the solution to run a few tests on the Review model and controller classes.

---
+++ GenerateReviewMVCTests/ReviewControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using GenerateReviewMVC.Controllers;
+using GenerateReviewMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Moq;
+
+namespace GenerateReviewMVCTests
+{
+    public class ReviewControllerTests
+    {
+        ILogger<HomeController> _logger;
+        IWebHostEnvironment _hostingEnvironment;
+        HomeController _homeController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _logger = Mock.Of<ILogger<HomeController>>();
+            _hostingEnvironment = Mock.Of<IWebHostEnvironment>();
+            _hostingEnvironment.WebRootPath = Directory.GetCurrentDirectory();
+            _homeController = new HomeController(_logger, _hostingEnvironment);
+        }
+
+        [Test]
+        public void CreateNewHomeControllerTest()
+        {
+            Assert.That(_homeController, Is.Not.Null);
+        }
+
+        [Test]
+        public void HomeControllerViewNameEqualsIndexTest()
+        {
+            ViewResult result = _homeController.Generate("Super Mario Cart") as ViewResult;
+            Assert.That(result.ViewName, Is.EqualTo("Index"));
+        }
+
+        [Test]
+        public void HomeControllerReturnsReviewObject()
+        {
+            ViewResult result = _homeController.Generate("Super Mario Cart") as ViewResult;
+            var model = result.ViewData.Model;
+            Assert.IsInstanceOf<Review>(model);
+        }
+    }
+}
---
+++ GenerateReviewMVCTests/ReviewModelTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using GenerateReviewMVC.Models;
+
+namespace GenerateReviewMVCTests
+{
+    public class ModelTests
+    {
+        [Test]
+        public void CreateReviewModelTest()
+        {
+            Review review = new Review();
+            Assert.That(review, Is.Not.Null);
+        }
+
+        [Test]
+        public void CreateReviewModelAndSetPropertiesTest()
+        {
+            Review review = new Review
+            {
+                Name = "Super Mario Cart",
+                Summary = "Great Game",
+                StarRating = 5
+            };
+
+            Assert.That(review, Is.Not.Null);
+            Assert.AreEqual(review.Name, "Super Mario Cart");
+            Assert.AreEqual(review.Summary, "Great Game");
+            Assert.AreEqual(review.StarRating, 5);
+        }
+    }
+}
