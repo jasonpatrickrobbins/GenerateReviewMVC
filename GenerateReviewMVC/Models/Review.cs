bc98b6abffe6dcd81d41aafe386fed878041f8c0
Jason Robbins <jasonrobbins@jasons-mbp-2.lan>
2021-09-13 00:47:07 -0400

    Added a Review.cs model class to the Models folder.

---
+++ GenerateReviewMVC/Models/Review.cs
@@ -0,0 +1,14 @@
+﻿namespace GenerateReview.Models
+{
+
+    /// <summary>    /// Model class for Reviews.    /// </summary>
+    public class Review
+    {
+        public string Overall { get; set; }
+        public bool Verified { get; set; }
+        public string ReviewTime { get; set; }
+        public string ReviewerID { get; set; }        public string ASIN { get; set; }        public string ReviewerName { get; set; }        public string ReviewText { get; set; }        public string Summary { get; set; }        public int UnixReviewTime { get; set; }
+        public string Name { get; set; }
+        public int StarRating { get; set; }
+    }
+}
